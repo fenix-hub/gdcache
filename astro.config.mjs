77bfaea69af8da7bd3d55e3671d3a2dc83397c9e
Nicolò Santilio <n.santilio97@gmail.com>
2022-11-22 13:47:37 +0000

    update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nicolosantilio.com/gdcache',
+	site: 'https://nicolosantilio.com',
   	base: '/gdcache',
 	integrations: [
 		// Enable Preact to support Preact JSX components.
