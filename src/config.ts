6fe8569f5fe4f4ab61dc324aed89d4190ac9e0f3
Nicolò Santilio <n.santilio97@gmail.com>
2023-02-13 16:28:14 +0000

    update sections

--- src/config.ts
+++ src/config.ts
@@ -49,9 +49,5 @@ export const SIDEBAR: Sidebar = {
 	en: {
 		'Section Header': [
 			{ text: 'Introduction', link: 'en/introduction' },
-			{ text: 'Page 2', link: 'en/page-2' },
-			{ text: 'Page 3', link: 'en/page-3' },
-		],
-		'Another Section': [{ text: 'Page 4', link: 'en/page-4' }],
 	},
 };
